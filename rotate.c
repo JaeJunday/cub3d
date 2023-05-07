5eb14c4a82d2358230a5824c90a196a02a6f06e8
Jaejun Kim <jaejkim@c1r6s5.42seoul.kr>
2023-05-07 22:58:55 +0900

    norm

--- rotate.c
+++ rotate.c
@@ -16,8 +16,12 @@ void	rotate(t_map *map, int dir)
 	dir_y = map->dir_y;
 	plane_x = map->plane_x;
 	plane_y = map->plane_y;
-	map->dir_x = cos(radian(ANGLE * dir)) * dir_x - sin(radian(ANGLE * dir)) * dir_y;
-	map->dir_y = sin(radian(ANGLE * dir)) * dir_x + cos(radian(ANGLE * dir)) * dir_y;
-	map->plane_x = cos(radian(ANGLE * dir)) * plane_x - sin(radian(ANGLE * dir)) * plane_y;
-	map->plane_y = sin(radian(ANGLE * dir)) * plane_x + cos(radian(ANGLE * dir)) * plane_y;
+	map->dir_x = \
+	cos(radian(ANGLE * dir)) * dir_x - sin(radian(ANGLE * dir)) * dir_y;
+	map->dir_y = \
+	sin(radian(ANGLE * dir)) * dir_x + cos(radian(ANGLE * dir)) * dir_y;
+	map->plane_x = \
+	cos(radian(ANGLE * dir)) * plane_x - sin(radian(ANGLE * dir)) * plane_y;
+	map->plane_y = \
+	sin(radian(ANGLE * dir)) * plane_x + cos(radian(ANGLE * dir)) * plane_y;
 }
